25cfe27ee8e77fe4e6bf2aad27f0717bffa1746d
MetaphysicsNecrosis <MetaphysicsNecrosis@DESKTOP-A9QI9P7>
2023-04-29 19:28:15 +0300

    Добавьте файлы проекта.

---
+++ GDTask-4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDTask_4
+{
+    class CodeMeDecodeYou
+    {
+        const int mod = 256;
+        static public char Coder(char c, int m)
+        {
+            return (char)(((int)(c) + m) % mod);
+        }
+        static public String Coder(String s, int m)
+        {
+            return String.Join("", (s.ToCharArray().Select(n => Coder(n, 5))).ToArray());
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = Console.ReadLine();
+            s = CodeMeDecodeYou.Coder(s, 5);
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
